8a4794b36a4a2b10e6ce465b4fa0c443ae7db062
Max Baraitser Smith <m.baraitsersmith@gmail.com>
2024-09-13 14:59:49 +0200

    latest

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -8,7 +8,14 @@ async function getStatus() {
         }
 
         const json = await response.json();
-        console.log(json);
+        const serverUrls = json.icestats.source.map((x) => x.listenurl.replace("http://locus.creacast.com:9001", "https://locus.creacast.com:9443") );
+        const dashboardUrls = $(".dashboard__stream-url");
+
+        dashboardUrls.each(function(index, element) {
+            if (serverUrls.includes(element.href) ) {
+                $(element).closest("tr").addClass("bold");
+            }
+        });
     } catch (error) {
         console.error(error.message);
     }
