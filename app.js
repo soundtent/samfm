38c38a9efa3678bdbb6dd80154cf96658c6ee672
Max Baraitser Smith <m.baraitsersmith@gmail.com>
2024-09-14 18:37:05 +0200

    latest

--- app.js
+++ app.js
@@ -26,8 +26,6 @@ const connectDB = require('./server/config/dbConfig');
 const initialisePassportLocalStrategy = require('./server/config/authConfig');
 
 const app = express();
-const httpPort = process.env.HTTP_PORT;
-const httpsPort = process.env.HTTPS_PORT;
 
 
 app.set("startDay", "2024-09-10"); //inclusive
@@ -97,11 +95,11 @@ app.use(require('./server/routes/authRoutes'));
 
 
 
-httpServer.listen(httpPort, () => {
-  console.log(`App listening on port ${httpPort} (http)`);
+httpServer.listen(80, () => {
+  console.log(`App listening on port 80 (http)`);
 });
 if (process.env.NODE_ENV == "production") {
-  httpsServer.listen(httpsPort, () => {
-    console.log(`App listening on port ${httpsPort} (https)`);
+  httpsServer.listen(443, () => {
+    console.log(`App listening on port 443 (https)`);
   });
 }
